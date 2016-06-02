6d9536f4c4d14a9bb50c35a98097dadeb5b32ad7
Hadrien Mens-Pellen <public@hadrienmp.fr>
2016-06-02 23:30:31 +0200

    FizzBuzz: Default numbers before refactoring

--- fizzbuzz/src/main/java/perso1/FizzBuzz.java
+++ fizzbuzz/src/main/java/perso1/FizzBuzz.java
@@ -4,7 +4,13 @@ package perso1;
  * Created by HadrienMP on 02/06/2016.
  */
 public class FizzBuzz {
-    public static String play(int i) {
+    public static String transform(int i) {
+        if (i==4) {
+            return "4";
+        }
+        if (i==2) {
+            return "2";
+        }
         return "1";
     }
 }
--- fizzbuzz/src/test/java/perso1/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/perso1/FizzBuzzTest.java
@@ -12,7 +12,23 @@ public class FizzBuzzTest {
 
     @Test
     public void one() {
-        String fizzbuzz = FizzBuzz.play(1);
+        String fizzbuzz = FizzBuzz.transform(1);
         assertThat(fizzbuzz).isEqualTo("1");
+    }
+
+    @Test
+    public void two() {
+        String fizzbuzz = FizzBuzz.transform(2);
+        assertThat(fizzbuzz).isEqualTo("2");
+    }
+
+    // TODO test 3
+    // TODO test null
+    // TODO test negative
+
+    @Test
+    public void four() {
+        String fizzbuzz = FizzBuzz.transform(4);
+        assertThat(fizzbuzz).isEqualTo("4");
     }
 }
