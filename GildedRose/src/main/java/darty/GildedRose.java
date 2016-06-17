4ab4d61cb38a6556a47abef4c654aa16091c9d10
Hadrien Mens-Pellen <public@hadrienmp.fr>
2016-06-17 12:13:52 +0200

    Darty/Gilded Rose: First session of refactor in mob

--- GildedRose/src/main/java/darty/GildedRose.java
+++ GildedRose/src/main/java/darty/GildedRose.java
@@ -4,6 +4,11 @@ package darty;
  * Created by HadrienMP on 02/06/2016.
  */
 public class GildedRose {
+    public static final String CONJURED = "Conjured Mana Cake";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final int QUALITY_MAX = 50;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -12,62 +17,60 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if ("Conjured Mana Cake".equals(items[i].name)) {
-                if(items[i].quality > 0) {
-                    items[i].quality = items[i].quality - 2;
+
+            // => Gestion du backstage passes & Aged Brie
+            if (items[i].name.equals(AGED_BRIE)
+                    || items[i].name.equals(BACKSTAGE_PASS)) {
+                if (items[i].quality < QUALITY_MAX) {
+                    items[i].quality = items[i].quality + 1;
                 }
-                items[i].sellIn = items[i].sellIn - 1;
-            } else {
-                if (!items[i].name.equals("Aged Brie")
-                        && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].quality > 0) {
-                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                } else {
-                    if (items[i].quality < 50) {
+                if (items[i].name.equals(BACKSTAGE_PASS)) {
+                    if (items[i].sellIn < 11 && items[i].quality < QUALITY_MAX) {
                         items[i].quality = items[i].quality + 1;
-
-                        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].sellIn < 11) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
-                                }
-                            }
-
-                            if (items[i].sellIn < 6) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
-                                }
-                            }
-                        }
                     }
-                }
 
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    items[i].sellIn = items[i].sellIn - 1;
-                }
 
-                if (items[i].sellIn < 0) {
-                    if (!items[i].name.equals("Aged Brie")) {
-                        if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].quality > 0) {
-                                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    items[i].quality = items[i].quality - 1;
-                                }
+                    if (items[i].sellIn < 6 && items[i].quality < QUALITY_MAX) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+
+
+                }
+            } else if (!items[i].name.equals(SULFURAS)) {
+                // produits standards (ne se bonifient)
+                items[i].quality = items[i].quality - 1;
+                if (CONJURED.equals(items[i].name)) {
+                    items[i].quality = items[i].quality - 1;
+                }
+            }
+
+
+            if (!items[i].name.equals(SULFURAS)) {
+                items[i].sellIn = items[i].sellIn - 1;
+            }
+
+            if (items[i].sellIn < 0) {
+                if (!items[i].name.equals(AGED_BRIE)) {
+                    if (!items[i].name.equals(BACKSTAGE_PASS)) {
+                        if (!items[i].name.equals(SULFURAS)) {
+                            items[i].quality = items[i].quality - 1;
+                            if (CONJURED.equals(items[i].name)) {
+                                items[i].quality = items[i].quality - 1;
                             }
-                        } else {
-                            items[i].quality = items[i].quality - items[i].quality;
                         }
                     } else {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
+                        items[i].quality = items[i].quality - items[i].quality;
+                    }
+                } else {
+                    if (items[i].quality < QUALITY_MAX) {
+                        items[i].quality = items[i].quality + 1;
                     }
                 }
             }
 
+            if (items[i].quality < 0) items[i].quality = 0;
+
         }
+
     }
 }
